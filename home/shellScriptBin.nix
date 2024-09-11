5839ce0fc028648a2e05a13907c6f4871627eec7
Kevin-2483 <3244577394@qq.com>
2024-09-12 02:43:06 +0800

    add some shell script bin

--- home/shellScriptBin.nix
+++ home/shellScriptBin.nix
@@ -1,6 +1,41 @@
 { pkgs, ... }: let
-  ny = pkgs.writeShellScriptBin "ny" ''${pkgs.nushell}/bin/nu -e yy'';
-  wny = pkgs.writeShellScriptBin "wny" ''${wezterm}/bin/nu ${ny}'';
+  ny = pkgs.writeShellScriptBin "ny" ''${pkgs.nushell}/bin/nu -e "yy '$@'"'';
+  kny = pkgs.writeShellScriptBin "kny" ''
+  if [ "$#" -eq 0 ]; then
+    ${pkgs.kitty}/bin/kitty ny "$HOME"
+  else
+    ${pkgs.kitty}/bin/kitty ny "$@"
+  fi
+  '';
+  wny = pkgs.writeShellScriptBin "wny" ''
+  if [ "$#" -eq 0 ]; then
+    ${pkgs.wezterm}/bin/wezterm -e ny "$HOME"
+  else
+    ${pkgs.wezterm}/bin/wezterm -e ny "$@"
+  fi
+  '';
+  wez = pkgs.writeShellScriptBin "wez" ''${pkgs.wezterm}/bin/wezterm "$@"'';
+  wtm = pkgs.writeShellScriptBin "wtm" ''
+  if [ "$#" -eq 0 ]; then
+     ${pkgs.tmux}/bin/tmux new-session -d -s default;${pkgs.wezterm}/bin/wezterm -e tmux attach -t default
+  else
+      ${pkgs.tmux}/bin/tmux new-session -d -s "$@";${pkgs.wezterm}/bin/wezterm -e tmux attach -t "$@"
+  fi
+  '';
+  ktm = pkgs.writeShellScriptBin "ktm" ''
+  if [ "$#" -eq 0 ]; then
+     ${pkgs.tmux}/bin/tmux new-session -d -s default;${pkgs.kitty}/bin/kitty tmux attach -t default
+  else
+      ${pkgs.tmux}/bin/tmux new-session -d -s "$@";${pkgs.kitty}/bin/kitty tmux attach -t "$@"
+  fi
+  '';
+  tm = pkgs.writeShellScriptBin "tm" ''
+  if [ "$#" -eq 0 ]; then
+     ${pkgs.tmux}/bin/tmux new-session -d -s default;${pkgs.tmux}/bin/tmux attach -t default
+  else
+      ${pkgs.tmux}/bin/tmux new-session -d -s "$@";${pkgs.tmux}/bin/tmux attach -t "$@"
+  fi
+  '';
 in {
-  home.packages = [ ny wny ];
+  home.packages = [ ny kny wny wez wtm ktm tm ];
 }
