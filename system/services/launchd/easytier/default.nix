63d0f8a5eafb7d1d8ab5b3c243883b21473af7cc
Kevin-2483 <3244577394@qq.com>
2024-10-08 21:10:53 +0800

    add easytier

--- system/services/launchd/easytier/default.nix
+++ system/services/launchd/easytier/default.nix
@@ -2,5 +2,11 @@
 {
     launchd.daemons.easytier = {
         command = "${pkgs.easytier}/bin/easytier-core -c /Users/kevin/.config/easytier/config.toml";
+        serviceConfig = {
+            RunAtLoad = true;
+            # KeepAlive = true;
+            StandardErrorPath = "/Users/kevin/.daemonlog/easytier.log";
+            StandardOutPath = "/Users/kevin/.daemonlog/easytier.log";
+        };
     };
 }
