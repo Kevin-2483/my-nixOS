1f84e6452160f6bd4b7d0fe0274bd572870bddc0
Kevin-2483 <3244577394@qq.com>
2025-02-14 18:17:29 +0800

    add mcsm

--- system/services/docker/mcsm/default.nix
+++ system/services/docker/mcsm/default.nix
@@ -1,6 +1,7 @@
 {
   virtualisation.oci-containers.containers."mcsm-daemon" = {
     image = "githubyumao/mcsmanager-daemon:latest";
+    autoStart = true;
     environment = {
       "MCSM_DOCKER_WORKSPACE_PATH" = "/home/kevin/services/docker/mcsm/daemon/data/InstanceData";
     };
@@ -17,6 +18,7 @@
   };
   virtualisation.oci-containers.containers."mcsm-web" = {
     image = "githubyumao/mcsmanager-web:latest";
+    autoStart = true;
     volumes = [
       "/etc/localtime:/etc/localtime:ro"
       "/home/kevin/services/docker/mcsm/web/data:/opt/mcsmanager/web/data:rw"
