d90bcf525e11940ef7e6984ad2c9055bb8904564
Kevin-2483 <3244577394@qq.com>
2025-02-19 16:47:20 +0800

    25.2.19

--- system/services/docker/mcsm/default.nix
+++ system/services/docker/mcsm/default.nix
@@ -10,9 +10,25 @@
       "/home/kevin/services/docker/mcsm/daemon/data:/opt/mcsmanager/daemon/data:rw"
       "/home/kevin/services/docker/mcsm/daemon/logs:/opt/mcsmanager/daemon/logs:rw"
       "/var/run/docker.sock:/var/run/docker.sock:rw"
+      "/home/kevin/mc:/Game:rw"
     ];
     ports = [
       "24444:24444/tcp"
+      "24444:24444/udp"
+      "24454:24454/tcp"
+      "24454:24454/udp"
+      "25565:25565/tcp"
+      "25565:25565/udp"
+      "25575:25575/tcp"
+      "25575:25575/udp"
+      "25585:25585/tcp"
+      "25585:25585/udp"
+      "25595:25595/tcp"
+      "25595:25595/udp"
+      "7007:7007/tcp"
+      "7007:7007/udp"
+      "9009:9009/tcp"
+      "9009:9009/udp"
     ];
     log-driver = "journald";
   };
